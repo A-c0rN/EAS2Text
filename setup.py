1d79098478bf37f1b77eb82cf2a9f6812df53e02
A-c0rN <mayer.brigan@gmail.com>
2022-01-23 17:14:54 -0600

    Update things

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='EAS2Text',
     packages=find_packages(include=['EAS2Text']),
-    version='0.1.0',
+    version='0.1.1',
     description='A Python library to convert raw EAS header data to a human readable text',
     author='A-c0rN',
     license='ODbL-1.0',
