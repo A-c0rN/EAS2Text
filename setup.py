d7295494c8a56619d11a61c2d0371f63f5734bc0
Anastasia Mayer <mayer.brigan@gmail.com>
2022-04-04 07:37:34 -0500

    Update Version in Setup  - Bring to 0.1.7

--- setup.py
+++ setup.py
@@ -1,38 +1,37 @@
 from os import path, pardir, chdir
 from setuptools import setup
 
-with open(path.join(path.dirname(__file__), 'README.md')) as readme:
+with open(path.join(path.dirname(__file__), "README.md")) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
 
 setup(
-    name='EAS2Text',
-    packages=['EAS2Text'],
-    version='0.1.6',
-    description='A Python library to convert raw EAS header data to a human readable text',
-    author='A-c0rN',
-    author_email='acrn@gwes-eas.network',
-    license='ODbL-1.0',
+    name="EAS2Text",
+    packages=["EAS2Text"],
+    version="0.1.7",
+    description="A Python library to convert raw EAS header data to a human readable text",
+    author="A-c0rN",
+    author_email="acrn@gwes-eas.network",
+    license="ODbL-1.0",
     install_requires=[],
     long_description=README,
-    long_description_content_type='text/markdown',
-    url='https://github.com/A-c0rN/EAS2Text',
-    keywords='eas alerting emergency-alert-system',
+    long_description_content_type="text/markdown",
+    url="https://github.com/A-c0rN/EAS2Text",
+    keywords="eas alerting emergency-alert-system",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: Other/Proprietary License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
+        "Development Status :: 5 - Production/Stable",
+        "License :: Other/Proprietary License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries",
-        'Topic :: Utilities',
-    ]
+        "Topic :: Utilities",
+    ],
 )
-
