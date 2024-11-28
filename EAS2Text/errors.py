92b42f43a9cddf6efc0da4f0062f2e22bfd69e89
A-c0rN <mayer.brigan@gmail.com>
2024-11-28 15:44:11 -0600

    Initial push, this does not work  - This does not work  - Repeat, this does not work.

---
+++ EAS2Text/errors.py
@@ -0,0 +1,17 @@
+class InvalidSAME(Exception):
+    def __init__(self, error, message="Invalid Data in SAME Message"):
+        self.message = message
+        self.error = error
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.message}: {self.error}"
+
+
+class MissingSAME(Exception):
+    def __init__(self, message="Missing SAME Message"):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.message}"
